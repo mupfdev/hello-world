1ee30c956f6080616b24c23fc14113a11af5721d
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-10-24 19:24:01 +0200

    Update build system, use FetchContent instead of ExternalProject_add

--- cmake/project_config.cmake
+++ cmake/project_config.cmake
@@ -4,5 +4,3 @@ set(PROJECT_TITLE "hello")
 
 set(project_sources
   ${CMAKE_CURRENT_SOURCE_DIR}/src/main.c)
-
-#configure_file(${CMAKE_CURRENT_SOURCE_DIR}/res/title.bmp ${CMAKE_CURRENT_SOURCE_DIR}/export COPYONLY)
